ee4c8ac82f645060c202e5f166eb90b0d2332781
vasromand <vasromand@gmail.com>
2014-05-04 22:45:39 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //Part 1
 var script = document.createElement('script');
-script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js';
+script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js';
 document.getElementsByTagName('head')[0].appendChild(script);
 //Part 2
 jQuery.noConflict();
